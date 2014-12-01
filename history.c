c2f62b07d1ca30a0f9a9e206e0e87b5f78889944
John B. Casey <jbc4988@rit.edu>
2014-11-30 23:11:55 -0500

    adds the print, add, and return command number functions, requires testing

--- history.c
+++ history.c
@@ -75,6 +75,8 @@ void freeNode(Node *begin){
 History *createHistory(){
 	History *newGuy = malloc(sizeof(History));
 	newGuy.max = MAX_HISTORY;
+	newGuy.current = BEGIN_HISTORY;
+	newGuy.currSize = BEGIN_SIZE;
 	newGuy->history = NULL;
 	return newGuy;
 }
@@ -89,9 +91,32 @@ int historySize(History *begin){
 	return returner;
 }
 
+void addToHistory(History *target, char *command){
+	addNode(target->history, command);
+	target.currSize++;
+	target.current++;
+	if(target.currSize > target.max){
+		Node *toFree = removal(target->history, target->history);
+		freeNode(target->history);
+		target.currSize--;
+	}
+}
+
+char *getCommand(History *target, int com){
+	Node *curr = target->history;
+	for(int i = (begin.current-begin.currentSize);i<=com;i++){
+		curr = curr->nod;
+	}
+	return curr->data;
+}
+
 void printStory(History *begin){
 	int size = historySize(begin);
-	
+	Node *curr = begin->history;
+	for(int i = (begin.current-begin.currentSize);i<=begin.currSize;i++){
+		printf("\t%d: %s", i, curr->data);
+		curr = curr->nod;
+	}
 }
 
 void freeHistory(History *tar){
