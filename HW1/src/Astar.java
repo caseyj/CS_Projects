c6f02b33fd01f869c3a2f3887c275f601f67c43c
JohnBCasey <caseyjohnb@gmail.com>
2016-02-14 17:11:11 -0500

    Initial commit of Astar.java. The purpose of this file is to create a working version of the algorithm and a test case and driver.

---
+++ HW1/src/Astar.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+
+
+/**
+ * @author Casey
+ * Title: Astar.java
+ * Language: Java 1.8
+ * Description: an implementation and test case of the A* algorithm
+ */
+public class Astar {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
