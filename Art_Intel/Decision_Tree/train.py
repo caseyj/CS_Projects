e5de8be79ed820b28bd235076d5e8a4836b4cf8b
JohnBCasey <caseyjohnb@gmail.com>
2016-04-11 14:12:11 -0400

    added the method to correctly read in a CSV

---
+++ Art_Intel/Decision_Tree/train.py
@@ -0,0 +1,23 @@
+import pickle
+import csv
+import numpy as np
+from Tree_Model import Tree
+import sys
+
+storage = list()
+with open('data.csv', 'rb') as inpu:
+	inputData = csv.reader('data.csv')
+	storage.append(next(inputData))
+	for row in inputData:
+		row[0] = float(row[0])
+		row[1] = float(row[1])
+		row[2] = float(row[2])
+		row[3] = float(row[3])
+		row[4] = float(row[4])
+		storage.append(row)
+#can use any package to read the file
+model =  Tree()
+model.train()
+with open('model.pkl','wb') as h:
+    pickle.load(model,h)
+
