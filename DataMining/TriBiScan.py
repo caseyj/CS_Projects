d39faf3588c180b466569ec72fc61b19b7ddacb3
JohnBCasey <caseyjohnb@gmail.com>
2016-05-02 23:08:33 -0400

    tested with file input, successfully finds all N-Grams from TXT file

--- DataMining/TriBiScan.py
+++ DataMining/TriBiScan.py
@@ -4,6 +4,7 @@ Author: John B. Casey jbc4988@rit.edu
 Description:
 Called By:
 '''
+import sys
 
 
 def DictWord(string, size_of_grams):
@@ -13,6 +14,10 @@ def DictWord(string, size_of_grams):
 	start_dex = 0
 	#set as the first edge, will be incremented
 	last_now = size_of_grams
+	#check if its too short to run and return whatever
+	string = string.lower()
+	if len(string) < size_of_grams:
+		graphs_words[string] = 1
 	#loop through straaaang
 	while last_now<=len(string):
 		#if its in the dictionary, just increment it
@@ -27,9 +32,39 @@ def DictWord(string, size_of_grams):
 	return graphs_words
 
 def DictCombine(list_of_strings, size_of_grams):
+	#a dictionary for every N-Gram
 	superDict = dict()
+	#loop through strings list
 	for i in list_of_strings:
+		#find the N-gram count for a single word
 		single = DictWord(i, size_of_grams)
+		#list of the grams
 		SingleKeys = list(single.keys())
+		#loop through the keylist and 
+		for j in SingleKeys:
+			#if its in the dictionary, add its grams
+			if j in superDict:
+				superDict[j] += single[j]
+			#otherwise, make a new key and add those grams
+			else:
+				superDict[j] = single[j]
+	return superDict
+
+
+
+
+
+
+
+
+
+
+with open(sys.argv[1], 'rb') as inpu:
+	listicle = inpu.readlines()
+bi_dict = DictCombine(listicle, 2)
+tri_dict = DictCombine(listicle, 3)
+print bi_dict
+
 
 print DictWord("hello", 2)
+print DictCombine(["the", "quick", "sly", "brown", "fox", "quickly", "to", "a"], 3)
