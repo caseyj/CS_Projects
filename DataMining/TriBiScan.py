9227751174a391cb8ec9e2a30a766afbd28226d7
JohnBCasey <caseyjohnb@gmail.com>
2016-05-02 22:49:44 -0400

    method to create dictionary for a single word by N-Grams works

--- DataMining/TriBiScan.py
+++ DataMining/TriBiScan.py
@@ -1,2 +1,35 @@
 '''
+Title: TriBiScan.py
+Author: John B. Casey jbc4988@rit.edu
+Description:
+Called By:
 '''
+
+
+def DictWord(string, size_of_grams):
+	#the dictionary containing N-gram counts for the given string
+	graphs_words = dict()
+	#where we start(usually zero, could be weird)
+	start_dex = 0
+	#set as the first edge, will be incremented
+	last_now = size_of_grams
+	#loop through straaaang
+	while last_now<=len(string):
+		#if its in the dictionary, just increment it
+		if string[start_dex:last_now] in graphs_words:
+			graphs_words[string[start_dex:last_now]] += 1
+		#otherwise, create a new key and set it to 1
+		else:
+			graphs_words[string[start_dex:last_now]] = 1
+		#increment the string indicies, because goddamn this shit isnt hard
+		start_dex+=1
+		last_now+=1
+	return graphs_words
+
+def DictCombine(list_of_strings, size_of_grams):
+	superDict = dict()
+	for i in list_of_strings:
+		single = DictWord(i, size_of_grams)
+		SingleKeys = list(single.keys())
+
+print DictWord("hello", 2)
